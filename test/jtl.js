51398061480d0df99c11d46b40b34df683590684
roylines <roy@roylines.co.uk>
2015-11-30 08:13:19 +0000

    initial version

---
+++ test/jtl.js
@@ -0,0 +1,9 @@
+const Lab = require("lab");
+const jtl = require("../lib/jtl");
+const lab = exports.lab = Lab.script();
+
+lab.experiment("jtl", () => {
+  lab.test("should", (done) => {
+    return done();
+  });
+});
