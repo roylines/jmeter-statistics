276af8e170777f3f366a5def022d43a44f54fc6d
roylines <roy@roylines.co.uk>
2015-11-30 16:22:55 +0000

    return apdex figures

--- test/jtl.js
+++ test/jtl.js
@@ -12,23 +12,29 @@ lab.experiment("jtl", () => {
     jtl(reader, writer, () => {
       var lines = writer.toString().split("\n");
       lines.should.be.length(4);
-      lines[0].should.equal('label,requestCount,meanResponseTimeMillis,meanRequestsPerSecond,apdex,errorPercentage');
+      lines[0].should.equal('label,requestCount,meanResponseTimeMillis,meanRequestsPerSecond,errorPercentage,apdex,satisfied,tolerating,frustrated');
       var line1 = lines[1].split(',');
 
       line1[0].should.equal('call1');
       line1[1].should.equal('4');
       line1[2].should.equal(((1131 + 1023 + 1170 + 24)/4).toString());
       line1[3].should.equal('3.46');
-      line1[4].should.equal('0.63');
-      line1[5].should.equal('0.25');
+      line1[4].should.equal('0.25');
+      line1[5].should.equal('0.63');
+      line1[6].should.equal('1');
+      line1[7].should.equal('3');
+      line1[8].should.equal('0');
       
       var line2 = lines[2].split(',');
       line2[0].should.equal('call2');
       line2[1].should.equal('4');
       line2[2].should.equal(((833 + 1907 + 2200 + 938)/4).toString());
       line2[3].should.equal('35.71');
-      line2[4].should.equal('0.38');
-      line2[5].should.equal('0');
+      line2[4].should.equal('0');
+      line2[5].should.equal('0.38');
+      line2[6].should.equal('0');
+      line2[7].should.equal('3');
+      line2[8].should.equal('1');
       return done();
     });
   });
